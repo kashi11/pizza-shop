f9f61ed1cd903e6b62864f6c8389eae676d3532d
Kashif Ali <kashifali.st0@gmail.com>
2021-02-02 20:47:52 +0500

    modified:   .eslintcache modified:   server/app.js

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.static("./build"));
+app.use(express.static("../build"));
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
