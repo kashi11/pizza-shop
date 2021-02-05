2d8ca592ff21846c1c912659da2cd112c71d1265
Kashif Ali <kashifali.st0@gmail.com>
2021-02-05 18:58:08 +0500

    modified:   src/redux/store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,9 +10,6 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f
   )
 );
 
